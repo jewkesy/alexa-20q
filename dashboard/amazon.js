1726575002bc178c1e9522a7a926bc348d405fbb
Daryl Jewkes <github@daryljewkes.com>
2017-01-12 20:30:28 +0000

    added amazon logic to get score and review count

--- dashboard/amazon.js
+++ dashboard/amazon.js
@@ -4,21 +4,27 @@ var request = require('request');
 var cheerio = require('cheerio');
 var console = require('tracer').colorConsole();
 var async = require('async');
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 
 const urlUK = "https://www.amazon.co.uk/Daryl-Jewkes-twenty-questions/dp/B01N6I0G8D";
 const urlUS =   "https://www.amazon.com/Daryl-Jewkes-twenty-questions/dp/B01N6I0G8D";
+var url = process.env.mongoURI;
 
-getRatings();
+exports.handler = function (event, context) {
+  getRatings();
+};
+
+// getRatings();
 
 function getRatings() {
-
-
 	async.parallel({
 	    uk: function(callback) {
 		    request(urlUK, function(err, response, html){
 		    	if (err) return callback(err);
-
+		    	// console.log(html)
 		    	var $ = cheerio.load(html, {ignoreWhitespace : true})
+		    	var robot = $('title').text();
+		    	if (robot == 'Robot Check') return callback(null, {});
 
 		    	var reviews = parseInt($('.a2s-review-star-count').text());
 		    	var score = $('.a-icon-star-small').attr().class.split(' ')[2];
@@ -27,49 +33,46 @@ function getRatings() {
 
 		    	return callback(null, {
 		    		score: score,
-					reviews: reviews
+					reviews: reviews,
+					url: urlUK
 				});
 		    });
 	    },
 	    us: function(callback) {
 	        request(urlUS, function(err, response, html){
 		    	if (err) return callback(err);
+		    	// return callback(null, {});
 		    	var $ = cheerio.load(html, {ignoreWhitespace : true})
-		    	console.log(html)
+		    	var robot = $('title').text();
+		    	if (robot == 'Robot Check') return callback(null, {});
+
 		    	var reviews = parseInt($('.a2s-review-star-count').text());
-		    	
-		    	console.log($('.a2s-pdd-reviews').html())
-
-
-
-		    	// var score = $('.a-icon-star-small').attr().class.split(' ')[2];
+		    	var score = $('.a-icon-star-small').attr().class.split(' ')[2];
 		    	score = score.replace("a-star-small-", "");
 		    	score = parseFloat(score.replace("-", "."));
 
 		    	return callback(null, {
 		    		score: score,
-					reviews: reviews
+					reviews: reviews,
+					url: urlUS
 				});
 		    });
 	    }
 	}, function(err, results) {
-		console.log(results)
-	    // results is now equals to: {one: 1, two: 2}
+		// console.log(results)
+		MongoClient.connect(url, function(err, db) {
+			var collection = db.collection('amazon');
+
+			collection.drop(function() {
+				results.dateTime = new Date();
+				collection.insertOne(results, function(err, result) {
+				    assert.equal(err, null);
+				    // callback(result);
+				    assert.equal(1, result.result.n);
+				    // console.log("Updated the document", newDoc);
+				    // callback(result);
+				}); 
+			});
+		});
 	});
-
-
-}
-
-function getScoreParts(html) {
-	console.log(html);
-
-	var score = 0;
-	var reviews = 0;
-	var top = "";
-
-	return {
-		score: score,
-		reviews: reviews,
-		top: top
-	}
 }
